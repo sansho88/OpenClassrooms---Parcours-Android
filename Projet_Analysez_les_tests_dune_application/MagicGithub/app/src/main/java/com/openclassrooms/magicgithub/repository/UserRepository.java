adb086d4b515ea21f2fcb43c5dd64c85fe57e7c3
Sansho <sansho88@hotmail.fr>
2019-12-07 17:23:32 +0100

    Add files via upload

--- Projet_Analysez_les_tests_dune_application/MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ Projet_Analysez_les_tests_dune_application/MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -1,9 +1,17 @@
 package com.openclassrooms.magicgithub.repository;
 
 import com.openclassrooms.magicgithub.api.ApiService;
+import com.openclassrooms.magicgithub.api.FakeApiService;
+import com.openclassrooms.magicgithub.api.FakeApiServiceGenerator;
 import com.openclassrooms.magicgithub.model.User;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS;
+import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS_RANDOM;
 
 public class UserRepository {
 
@@ -13,16 +21,31 @@ public class UserRepository {
         this.apiService = apiService;
     }
 
-    public List<User> getUsers() {
+
+
+     public List<User> getUsers() {
         // TODO: A modifier
-        return null;
+
+         return apiService.getUsers(); // VVVVV
+
+
+
     }
 
     public void generateRandomUser() {
         // TODO: A modifier
+
+         apiService.generateRandomUser(); //VVVVVVVVVV
+
+
     }
+
 
     public void deleteUser(User user) {
         // TODO: A modifier
+        apiService.deleteUser(user); // VVVVVVVVVVVVVVVVV
+
+
+
     }
 }
